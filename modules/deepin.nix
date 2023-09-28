2666acb39eb41fb0e6a2f058b46b6c67c6c7a24b
Demine0 <demine0@icloud.com>
2023-09-29 01:38:47 +0300

    update

--- modules/deepin.nix
+++
@@ -1,5 +0,0 @@
-{ pkgs, lib, config, ... }:
-{
-   services.xserver.desktopManager.deepin.enable = true;
-   environment.sessionVariables.NIX_GSETTINGS_OVERRIDES_DIR = lib.mkForce "/share/gsettings-schemas/nixos-gsettings-overrides/glib-2.0/schemas";
-}
--- modules/pantheon.nix
+++
@@ -1,5 +0,0 @@
-{ lib, pkgs, config, ...}:
-{
- #programs.evince.enable = lib.mkForce false;
- #services.xserver.desktopManager.pantheon.enable = true; 
-}
