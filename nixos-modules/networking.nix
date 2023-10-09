3f72295127d3144e3646385ae72b0a4f62f9fe7b
Demine0 <demine0@icloud.com>
2023-10-09 16:24:46 +0300

    wireguard fix on network manager

--- nixos-modules/networking.nix
+++ nixos-modules/networking.nix
@@ -3,6 +3,9 @@
   networking = {
 	wireless.enable = false; # Enables wireless support via wpa_supplicant.
   	useDHCP = false;
-  	networkmanager.enable = true; };
-	services.wg-netmanager.enable = true;
-}
+  	networkmanager.enable = true;
+        wireguard.enable = true;
+        firewall.checkReversePath = "loose";
+      };
+        services.wg-netmanager.enable = true;
+    }
