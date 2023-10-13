f06ad7c4c495f796a1dcb4e0b0545eca54f9e0d5
Demine0 <demine0@icloud.com>
2023-10-13 11:17:38 +0300

    nvim colorschemes and plugins

--- all/nixvim.nix
+++ all/nixvim.nix
@@ -2,6 +2,13 @@
 {                              
   programs.nixvim =            
   { enable = true;
+  colorschemes = { 
+    base16 = { 
+      enable = true;
+      package = pkgs.vimPlugins.nvim-base16;
+      colorscheme = "material-darker";
+    };
+  };
     plugins = { 
       treesitter = {
         enable = true;
@@ -9,7 +16,9 @@
       treesitter-context.enable = true;
       lsp.enable = true;
       lsp-lines.enable = true;
+      lastplace.enable = true;
       rainbow-delimiters.enable = true;
+      indent-blankline.enable = true;
       nvim-osc52.enable = true;
       presence-nvim.enable = true;
       nix.enable = true;
